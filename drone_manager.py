18703f911ada6b174c6eaf44ca35b2097c999acc
Cleby Francisco <cfs7@softex.cin.ufpe.br>
2024-11-10 08:17:04 -0300

    set speed

--- drone_manager.py
+++ drone_manager.py
@@ -8,43 +8,52 @@ logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 logger = logging.getLogger(__name__)
 
 DEFAULT_DISTANCE = 0.30
+DEFAULT_SPEED = 10
+
 
 class DroneManager(object):
-    def __init__(self, host_ip='192.168.10.2', host_port=8889, drone_ip='192.168.10.1', drone_port=8889, is_imperial=False):
-        self.host_ip = host_ip 
+    def __init__(
+        self,
+        host_ip="192.168.10.2",
+        host_port=8889,
+        drone_ip="192.168.10.1",
+        drone_port=8889,
+        is_imperial=False,
+        speed=DEFAULT_SPEED,
+    ):
+        self.host_ip = host_ip
         self.host_port = host_port
         self.drone_ip = drone_ip
         self.drone_port = drone_port
         self.drone_address = (drone_ip, drone_port)
-        self.is_imperial =  is_imperial
+        self.is_imperial = is_imperial
+        self.speed = speed
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.bind((self.host_ip, self.host_port))
 
-        # self.socket.sendto(b'command', self.drone_address)
-        # self.socket.sendto(b'streamon', self.drone_address)
-        
         self.response = None
         self.stop_event = threading.Event()
-        self._response_thread = threading.Thread(target=self.receive_response, args=(self.stop_event, ))
-                                           
+        self._response_thread = threading.Thread(
+            target=self.receive_response, args=(self.stop_event,)
+        )
         self._response_thread.start()
-        
-        self.send_command('command')
-        self.send_command('streamon')
-        
+
+        self.send_command("command")
+        self.send_command("streamon")
+        self.set_speed(self.speed)
+
     def receive_response(self, stop_event):
         while not stop_event.is_set():
             try:
                 self.response, ip = self.socket.recvfrom(3000)
-                logger.info({'action':'receive_response','response':self.response})
+                logger.info({"action": "receive_response", "response": self.response})
             except socket.error as ex:
-                logger.error({'action': 'receive_reponse', 'ex': ex})
+                logger.error({"action": "receive_response", "ex": ex})
                 break
-        
-        
+
     def __dell__(self):
         self.stop()
-            
+
     def stop(self):
         self.stop_event.set()
         retry = 0
@@ -54,64 +63,67 @@ class DroneManager(object):
                 break
             retry += 1
         self.socket.close()
-            
+
     def send_command(self, command):
-        logger.info({'action':'send_command', 'command':command})
-        self.socket.sendto(command.encode('utf-8'), self.drone_address)
-        
+        logger.info({"action": "send_command", "command": command})
+        self.socket.sendto(command.encode("utf-8"), self.drone_address)
+
         retry = 0
         while self.response is None:
             time.sleep(0.3)
             if retry > 3:
                 break
             retry += 1
+
         if self.response is None:
             response = None
         else:
-            reponse = self.response.decode('utf-8')
+            response = self.response.decode("utf-8")
         self.response = None
         return response
-            
+
     def takeoff(self):
-        return self.send_command('takeoff')
-            
+        return self.send_command("takeoff")
+
     def land(self):
-        return self.send_command('land')
-    
+        return self.send_command("land")
+
     def move(self, direction, distance):
-        distance= float(distance)
+        distance = float(distance)
         if self.is_imperial:
             distance = int(round(distance * 30.48))
         else:
             distance = int(round(distance * 100))
-            
-        return self.send_command(f'{direction} {distance}')
-    
+        return self.send_command(f"{direction} {distance}")
+
     def up(self, distance=DEFAULT_DISTANCE):
-        return self.move('up', distance)
-    
+        return self.move("up", distance)
+
     def down(self, distance=DEFAULT_DISTANCE):
-        return self.move('down', distance)
-    
+        return self.move("down", distance)
+
     def left(self, distance=DEFAULT_DISTANCE):
-        return self.move('left', distance)
-    
+        return self.move("left", distance)
+
     def right(self, distance=DEFAULT_DISTANCE):
-        return self.move('right', distance)
-    
+        return self.move("right", distance)
+
     def forward(self, distance=DEFAULT_DISTANCE):
-        return self.move('forward', distance)
-    
+        return self.move("forward", distance)
+
     def back(self, distance=DEFAULT_DISTANCE):
-        return self.move('back', distance)
-    
-            
-if __name__ == '__main__':
+        return self.move("back", distance)
+
+    def set_speed(self, speed):
+        return self.send_command(f"speed {speed}")
+
+
+if __name__ == "__main__":
     drone_manager = DroneManager()
+
+    drone_manager.set_speed(100)
     drone_manager.takeoff()
-    
     time.sleep(10)
-    
     drone_manager.forward()
     time.sleep(5)
     drone_manager.right()
@@ -120,14 +132,11 @@ if __name__ == '__main__':
     time.sleep(5)
     drone_manager.left()
     time.sleep(5)
+    drone_manager.set_speed(10)
+    time.sleep(1)
     drone_manager.up()
     time.sleep(5)
     drone_manager.down()
     time.sleep(5)
-    
     drone_manager.land()
-    time.sleep(2)
     drone_manager.stop()
-            
-        
-        
